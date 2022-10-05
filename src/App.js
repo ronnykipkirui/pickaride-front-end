1f23be8174f9d1d073c989a888c1f6433a3c3ed4
Ronny <ronnybet41@gmail.com>
2022-10-05 18:29:30 +0300

    style home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./header/Header";
+import Home from "./home/Home";
+import TopBar from "./topbar/TopBar";
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
-}
+function App(){
+  return(
+    <>
+    <TopBar/>
+    <Header/>
+    <Home/>
+
+    </>
+  )
+} 
+
 
 export default App;
