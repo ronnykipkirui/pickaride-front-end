38f0d54c1ade1aa73b076763cd7b29ded28b7a5f
Ronny <ronnybet41@gmail.com>
2022-10-05 18:27:25 +0300

    add header component

---
+++ src/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className="header ">
+         <div className="headertittle">
+            <span className="headtittlesm">React & node</span>
+            <span className="headtittlelg">Blog</span>
+         </div>
+         <img
+         className="headerimg"
+         src=""
+         alt=""
+         ></img>
+    </div>
+  )
+}
