a682ed17633fd2822587f36e9200fdfe09642fc1
Ronny <ronnybet41@gmail.com>
2022-10-05 18:30:23 +0300

    style header

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,30 @@
+import "./topbar.css"
+function TopBar() {
+
+    return (
+        <div className="top">
+
+
+            <div className="topLeft">
+                <i className="topicon fa-brands fa-facebook"></i>
+                <i className="topicon fa-brands fa-twitter"></i>
+                <i className="topicon fa-brands fa-instagram"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">Home</li>
+                    <li className="topListItem">About</li>
+                    <li className="topListItem">Contact</li>
+                    <li className="topListItem">Write</li>
+
+                </ul>
+            </div>
+            <div className="topRight">
+                <img className="topImg" src="/public/asset/ava.jpeg" alt="icon"></img>
+                <i className="topsearch icon fa-solid fa-magnifying-glass"></i>
+            </div>
+
+        </div>
+    )
+}
+export default TopBar
